6d022a2ff195d22efb5d070b6fccd73ccca779a4
Samson Zhou <samsonzhou@gmail.com>
2026-09-14 23:14:01 -0500

    Update template.py

--- cp/template.py
+++ cp/template.py
@@ -1,6 +1,6 @@
 import sys
 #from collections import defaultdict, deque, Counter
-#import heapq
+import heapq
 import math
 
 # Overwrite standard input for fast I/O
@@ -34,33 +34,6 @@ if __name__ == '__main__':
     for _ in range(t):
         solve()
 
-#Booth's algorithm
-#Finds first lexicographically ordered cyclic shift of a string s
-#Essentially iterate over ss (s repeated twice) and KMP-style search
-def least_rotation(s):
-    s = s + s
-    n = len(s) // 2
-
-    i, j, k = 0, 1, 0
-
-    while i < n and j < n and k < n:
-        if s[i + k] == s[j + k]:
-            k += 1
-            continue
-
-        if s[i + k] > s[j + k]:
-            i = i + k + 1
-            if i <= j:
-                i = j + 1
-        else:
-            j = j + k + 1
-            if j <= i:
-                j = i + 1
-
-        k = 0
-
-    start = min(i, j)
-    return s[start:start + n]
 
 #Subroutine for creating a list of all primes up to n
 #Sieve approach, removes all multiples, runtime O(n log log n)
@@ -75,30 +48,6 @@ def all_divs_up_to(n):
     ls[1]=1
     primes=[j for j in range(n) if ls[j]==-1]
     return ls
-
-#from tryingoutcp
-def dfs_iterative(graph, root, parent, sz):
-    n = len(graph)
- 
-    visited = [0] * n
-    order = []
-    stack = [root]
-    visited[root] = 1
-    while stack:
-        node = stack.pop()
-        order.append(node)
- 
-        for neighbor in graph[node]:
-            if not visited[neighbor]:
-                visited[neighbor] = 1
-                parent[neighbor] = node
-                stack.append(neighbor)
- 
-    for node in reversed(order):
-        sz[node] = 1
-        for neighbor in graph[node]:
-            if neighbor != parent[node]:
-                sz[node] += sz[neighbor]
 
 def factorial_list(n, MOD):
     factorial = [1]
@@ -116,3 +65,23 @@ def choose(n, k, MOD):
     num = factorial_val(n)
     denom = factorial_val(k) * factorial_val(n-k) % MOD
     return (num * pow(denom, -1, MOD)) % MOD
+
+def dijkstra(n,start,edges):
+    MAX = pow(10,10)
+    dist = [MAX]*n
+    adj=[[] for _ in range(n)]
+    for (u,v,w) in edges:
+        adj[u-1].append((v-1,w))
+        adj[v-1].append((u-1,w))
+    q=[(0,start-1)]
+    heapq.heapify(q)
+    fin=[0]*n
+    while q:
+        (d,v)=heapq.heappop(q)
+        if fin[v]==0:
+            fin[v]=1
+            dist[v]=d
+        for (u,w) in adj[v]:
+            heapq.heappush(q,(d+w,u))
+    return dist
+                
